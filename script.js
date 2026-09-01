b589df53e2915e7fb2b34b8183c7f3adbe3b1d19
Alyzza Marie <sabioalyzzamarie@gmail.com>
2026-09-01 16:05:34 +0800

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const memories = [
     {
         number: "01",
         title: "The Beginning",
-        image: "images/photo1.PNG",
+        image: "photo1.PNG",
         message:
             "Our very first picture together. I was still so shy, trying to look cute and innocent, but underneath all of that, I was already so happy."
     },
@@ -15,7 +15,7 @@ const memories = [
     {
         number: "02",
         title: "Your Hand",
-        image: "images/photo2.PNG",
+        image: "photo2.PNG",
         message:
             "Yes, I secretly took a picture of your hand HAHAHA. I literally just wanted to show my friends because I was excited about you."
     },
@@ -23,7 +23,7 @@ const memories = [
     {
         number: "03",
         title: "Our little joyride",
-        image: "images/photo3.jpg",
+        image: "photo3.jpg",
         message:
             "You were driving and I secretly took this picture because I wanted to keep the moment. We were just on our way somewhere, but somehow being beside you already made an ordinary ride feel like something worth remembering."
     },
@@ -31,7 +31,7 @@ const memories = [
     {
         number: "04",
         title: "The Firsts",
-        image: "images/photo4.jpg",
+        image: "photo4.jpg",
         message:
             "This one will always be special to me. It was my first time being this close to someone in this way, and somehow, it felt natural with you. My first cuddles with you. Our first kiss. I remember the feeling more than anything — that moment when I realized that I was becoming more comfortable with you. It felt like somewhere I wanted to stay."
     },
@@ -39,7 +39,7 @@ const memories = [
     {
         number: "05",
         title: "Our First Dinner Date",
-        image: "images/photo5.jpg",
+        image: "photo5.jpg",
         message:
             "Nothing extravagant, just dinner with you. But I remember how happy I was that I got to sit with you, talk a little longer, and make the day last just a little more."
     },
@@ -47,7 +47,7 @@ const memories = [
     {
         number: "06",
         title: "The Day It Became 'Us'",
-        image: "images/photo6.jpg",
+        image: "photo6.jpg",
         message:
             "Our second meet. Lunch, arcade games, laughing, and a whole lot of memories in one day. And then I finally said the words: “Be my boyfriend.” You said yes. This is probably one of my favorite days because this was the day everything changed."
     },
@@ -55,7 +55,7 @@ const memories = [
     {
         number: "07",
         title: "Us, Holding Hands",
-        image: "images/photo7.jpg",
+        image: "photo7.jpg",
         message:
             "I never needed someone to hold my hand. I just loved that you always chose to offer yours. There's something about that little gesture that makes me feel cared for, protected, and loved. It's one of the smallest things you do, but it means so much to me."
     },
@@ -63,7 +63,7 @@ const memories = [
     {
         number: "08",
         title: "Your Hand in Mine",
-        image: "images/photo8.jpg",
+        image: "photo8.jpg",
         message:
             "You held my hand and took a picture of it, and I think that's why I love this so much."
     },
@@ -71,7 +71,7 @@ const memories = [
     {
         number: "09",
         title: "Wherever We Go",
-        image: "images/photo9.jpg",
+        image: "photo9.jpg",
         message:
             "There's something about going somewhere with you that makes even an ordinary ride feel special. Maybe it's because I'm with you. Just another little memory that I know I'll look back on and smile."
     },
@@ -79,7 +79,7 @@ const memories = [
     {
         number: "10",
         title: "Caught You",
-        image: "images/photo10.jpg",
+        image: "photo10.jpg",
         message:
             "Okay, fine. I secretly took this because you looked handsome. I couldn't help it HAHAHA. And yes, I probably will keep secretly taking pictures of you."
     }
