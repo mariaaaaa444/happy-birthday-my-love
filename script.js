ac6413f3025aa9b1eb3a05c3d338e5c60b705ef7
Alyzza Marie <sabioalyzzamarie@gmail.com>
2026-09-01 16:09:03 +0800

    script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const memories = [
     {
         number: "03",
         title: "Our little joyride",
-        image: "photo3.jpg",
+        image: "photo3.JPG",
         message:
             "You were driving and I secretly took this picture because I wanted to keep the moment. We were just on our way somewhere, but somehow being beside you already made an ordinary ride feel like something worth remembering."
     },
@@ -31,7 +31,7 @@ const memories = [
     {
         number: "04",
         title: "The Firsts",
-        image: "photo4.jpg",
+        image: "photo4.JPG",
         message:
             "This one will always be special to me. It was my first time being this close to someone in this way, and somehow, it felt natural with you. My first cuddles with you. Our first kiss. I remember the feeling more than anything — that moment when I realized that I was becoming more comfortable with you. It felt like somewhere I wanted to stay."
     },
@@ -39,7 +39,7 @@ const memories = [
     {
         number: "05",
         title: "Our First Dinner Date",
-        image: "photo5.jpg",
+        image: "photo5.JPG",
         message:
             "Nothing extravagant, just dinner with you. But I remember how happy I was that I got to sit with you, talk a little longer, and make the day last just a little more."
     },
@@ -47,7 +47,7 @@ const memories = [
     {
         number: "06",
         title: "The Day It Became 'Us'",
-        image: "photo6.jpg",
+        image: "photo6.JPG",
         message:
             "Our second meet. Lunch, arcade games, laughing, and a whole lot of memories in one day. And then I finally said the words: “Be my boyfriend.” You said yes. This is probably one of my favorite days because this was the day everything changed."
     },
@@ -55,7 +55,7 @@ const memories = [
     {
         number: "07",
         title: "Us, Holding Hands",
-        image: "photo7.jpg",
+        image: "photo7.JPG",
         message:
             "I never needed someone to hold my hand. I just loved that you always chose to offer yours. There's something about that little gesture that makes me feel cared for, protected, and loved. It's one of the smallest things you do, but it means so much to me."
     },
@@ -63,7 +63,7 @@ const memories = [
     {
         number: "08",
         title: "Your Hand in Mine",
-        image: "photo8.jpg",
+        image: "photo8.JPG",
         message:
             "You held my hand and took a picture of it, and I think that's why I love this so much."
     },
@@ -71,7 +71,7 @@ const memories = [
     {
         number: "09",
         title: "Wherever We Go",
-        image: "photo9.jpg",
+        image: "photo9.JPG",
         message:
             "There's something about going somewhere with you that makes even an ordinary ride feel special. Maybe it's because I'm with you. Just another little memory that I know I'll look back on and smile."
     },
@@ -79,7 +79,7 @@ const memories = [
     {
         number: "10",
         title: "Caught You",
-        image: "photo10.jpg",
+        image: "photo10.JPG",
         message:
             "Okay, fine. I secretly took this because you looked handsome. I couldn't help it HAHAHA. And yes, I probably will keep secretly taking pictures of you."
     }
